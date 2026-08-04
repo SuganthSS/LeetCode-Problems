4bd46a8fee4292e24fb27c28d3fd57b6ba29ae42
Suganth <suganth2873@gmail.com>
2026-08-04 12:09:00 +0530

    Time: 2 ms (33.88%), Space: 45.3 MB (90.54%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> list=new ArrayList<>();
+        List<Integer> temp=new ArrayList<>();
+        boolean[] visited=new boolean[nums.length];
+        backtrack(nums,list,temp,visited);
+        return list;
+    }
+
+    public static void backtrack(int[] nums,List<List<Integer>> list,List<Integer> temp,boolean[] visited)
+    {
+        if(temp.size()==nums.length)
+        {
+            list.add(new ArrayList<>(temp));
+            return;
+        }
+
+        for(int i=0;i<nums.length;i++)
+        {
+            if(visited[i]) continue;
+
+            visited[i]=true;
+            temp.add(nums[i]);
+
+            backtrack(nums, list, temp, visited);
+
+            temp.remove(temp.size()-1);
+            visited[i]=false;
+        }
+    }
+}
