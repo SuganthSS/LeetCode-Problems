c56f34f60ab22273c97429bc74e833a07c213881
Suganth <suganth2873@gmail.com>
2026-08-13 15:10:24 +0530

    Time: 8 ms (91.73%), Space: 49.1 MB (61.09%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,16 +1,16 @@
 class Solution {
-    static{
-        Runtime.getRuntime().addShutdownHook(new Thread(()->{
-            try(java.io.FileWriter f = new java.io.FileWriter("display_runtime.txt"))
-            {
-                f.write("0");
-            }
-            catch(Exception e)
-            {
+    // static{
+    //     Runtime.getRuntime().addShutdownHook(new Thread(()->{
+    //         try(java.io.FileWriter f = new java.io.FileWriter("display_runtime.txt"))
+    //         {
+    //             f.write("0");
+    //         }
+    //         catch(Exception e)
+    //         {
                 
-            }
-        }));
-    }
+    //         }
+    //     }));
+    // }
     public int[][] merge(int[][] intervals) {
         if(intervals.length <=1)
         {
