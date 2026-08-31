b1e47c4173df75edae7697e1d363edb1391298e5
Suganth <suganth2873@gmail.com>
2026-08-31 12:00:18 +0530

    Time: 0 ms (100%), Space: 42.2 MB (98.54%) - LeetHub

--- 0500-keyboard-row/0500-keyboard-row.java
+++ 0500-keyboard-row/0500-keyboard-row.java
@@ -1,27 +1,23 @@
 class Solution {
     public String[] findWords(String[] words) {
-        String l1="qwertyuiop";
-        String l2="asdfghjkl";
-        String l3="zxcvbnm";
-        List<String> list=new ArrayList<>();
-        for(String s:words)
-        {
-            String temp=s;
-            s=s.toLowerCase();
-            if(s.chars().allMatch(c -> l1.indexOf(c) >= 0))
-            {
-                list.add(temp);
-            }
-            else if(s.chars().allMatch(c -> l2.indexOf(c) >= 0))
-            {
-                list.add(temp);
-            }
-            else if(s.chars().allMatch(c -> l3.indexOf(c) >= 0))
-            {
-                list.add(temp);
+        ArrayList<String> list = new ArrayList<>();
+        String first  = "qwertyuiop";
+        String second = "asdfghjkl";
+        String third  = "zxcvbnm";
+
+        for (String word : words) {
+            if (isinrow(word, first) || isinrow(word, second) || isinrow(word, third)) {
+                list.add(word);
             }
         }
-        String[] arr=list.toArray(new String[0]);
-        return arr;
+
+        return list.toArray(new String[0]); // cleaner than manual array copy
+    }
+
+    private boolean isinrow(String s, String row) {
+        for (char c : s.toCharArray()) {
+            if (row.indexOf(Character.toLowerCase(c)) == -1) return false;
+        }
+        return true;
     }
 }
